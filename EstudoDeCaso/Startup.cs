d1e3e7fc55af1bb5daadd8925edb7b6102078061
IcaroCleto <icarocleto.sousa@outlook.com>
2016-10-10 16:45:48 -0300

    Initial Commit

---
+++ EstudoDeCaso/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstudoDeCaso.Startup))]
+namespace EstudoDeCaso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
